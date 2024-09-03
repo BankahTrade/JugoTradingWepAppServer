340a4ecad6945f94f323004d0b2b511e3886c3aa
Bankah Anthony Bekoe <107210739+bankah-junior@users.noreply.github.com>
2024-09-03 11:54:38 +0000

    Update server.js

--- server.js
+++ server.js
@@ -11,7 +11,7 @@ const app = express();
 // Middlewares
 app.use(express.json());
 app.use(cors({
-    origin: ['http://localhost:5173', 'http://localhost:5174', 'http://localhost:3000'],
+    origin: ['http://localhost:5173', 'https://jugotrading.netlify.app/', 'http://localhost:3000'],
   }));
 
 app.use((req, res, next) => {
