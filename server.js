a874a457acf6a6a13a7cdaffa0c63eca4309e46f
Bankah Anthony Bekoe <154749514+bankah@users.noreply.github.com>
2024-09-03 11:25:42 +0000

    Initial Commit

---
+++ server.js
@@ -0,0 +1,37 @@
+require('dotenv').config()
+
+const express = require('express');
+const mongoose = require('mongoose');
+const userRoutes = require('./routes/userRoute');
+const cors = require('cors');
+
+// Creating Express app
+const app = express();
+
+// Middlewares
+app.use(express.json());
+app.use(cors({
+    origin: ['http://localhost:5173', 'http://localhost:5174', 'http://localhost:3000'],
+  }));
+
+app.use((req, res, next) => {
+    console.log(req.path, req.method)
+    next()
+})
+
+// Routes
+app.use('/api/user', userRoutes);
+
+const port = process.env.PORT || 4000;
+
+// Connection to database
+mongoose.connect(process.env.MONGO_URI)
+    .then(() => {
+        // Listening for request
+        app.listen(port, () => {
+            console.log(`Server running on port ${port} & MongoDB conected!!`);
+        })
+    })
+    .catch((error) => {
+        console.log(error);
+    })
